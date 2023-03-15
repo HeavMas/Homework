614d50b630d9042fbb4ffab54966435313914b5c
HeavMas <94596205+HeavMas@users.noreply.github.com>
2023-03-15 15:48:07 +0300

    Update prog3.py

--- threadpool/prog3.py
+++ threadpool/prog3.py
@@ -3,4 +3,33 @@
 Создайте функцию которая создает txt файл  по пути из очереди.
 Запустите все в разных потоках.
 """
-"Здесь должна быть программа, но пока её нет"
+from threading import Thread
+from queue import Queue
+import os
+w = input()
+def way(queue):
+    with open('Names.txt', 'r', encoding='UTF-8') as f:
+        a = f.read().split('\n')
+    queue.put(a)
+
+def repl(queue):
+    lst = queue.get()
+    for i in range(6):
+        with open(f'{w + lst[i] +  ".txt"}', 'w') as f:
+            f.write('файл' + str(i))
+    queue.task_done()
+
+
+
+def main():
+    queue = Queue()
+    Impname_thread = Thread(target=way, args=(queue, ))
+    Impname_thread.start()
+    Done_thread = Thread(target=repl, args=(queue, ), daemon=True)
+    Done_thread.start()
+    Impname_thread.join()
+    queue.join()
+
+if __name__ == '__main__':
+    main()
+

73c551aac16ce55fbefc2474c660aa3272febca2
HeavMas <94596205+HeavMas@users.noreply.github.com>
2023-03-15 15:47:30 +0300

    Update prog4.py

--- thread/prog4.py
+++ thread/prog4.py
@@ -3,7 +3,17 @@
 Запустите функцию для каждого файла в отдельном потоке.
 Измерьте время выполнения программы.
 """
+
+import time
 import os
 from threading import Thread
-
-
+way = input()
+def repl(i):
+    with open(f'{way + "file" + str(i) + ".txt"}', 'r') as f:
+        id = f.read()
+    id = id.replace('ids', 'id')
+    with open(f'{way + "file" + str(i) + ".txt"}', 'w') as f:
+        f.write(id)
+for i in range(10):
+    th = Thread(target=repl(i), args=(i, ))
+    th.start()

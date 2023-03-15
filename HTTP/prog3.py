0093d22b8b7cdd5a4727bcb6a68dafb96bcf8d8f
HeavMas <94596205+HeavMas@users.noreply.github.com>
2023-03-15 15:50:39 +0300

    Add files via upload

---
+++ HTTP/prog3.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = "https://rickandmortyapi.com/api/character"
+response = requests.get(url, params={"page": 2})
+data = json.loads(response.text)
+for character in data["results"][14:74]:
+    name = character["name"]
+    planet = character["origin"]["name"]
+    episodes = character["episode"]
+    print(f"{name} ({planet}):")
+    print("\tЭпизоды:")
